245cdd2694f3c3974afe21e8979df8fabafb58c5
uppa1234 <83637228+uppa1234@users.noreply.github.com>
2024-01-26 15:54:39 +0700

    Add files via upload

--- polars_utils.py
+++ polars_utils.py
@@ -2,7 +2,7 @@ import polars as pl
 
 # General functions
 
-def scan_file(path:str) -> pl.LazyFrame:
+def scan_file(path: str) -> pl.LazyFrame:
     '''
     Lazily scans a .parquet(.gzip), .csv or .txt file with a delimiter of , or $$$ with all columns as str
     Assumes no single $ in text.
@@ -26,25 +26,52 @@ def scan_file(path:str) -> pl.LazyFrame:
     # Read ,
     return pl.scan_csv(path, separator=',', infer_schema_length=0)
 
-def parse_dates(lf:pl.LazyFrame, date_col: str='D001KEY', format: str='%Y%m%d') -> pl.LazyFrame:
+def parse_dates(lf:pl.LazyFrame, date_col: str='D001KEY') -> pl.LazyFrame:
     '''Parses a single date column in a DataFrame/LazyFrame.'''
     dtype = lf.select(pl.col(date_col)).dtypes[0]
+    formats = ['%Y%m%d'] # TODO add all weird date formats we have (not time)
 
     # Already good if type is Date
     if dtype == pl.Date:
         return lf
 
     # Change Datetime to just Date
-    if lf.select(date_col).dtypes[0] == pl.Datetime:
+    if dtype == pl.Datetime:
         return lf.with_columns(pl.col(date_col).dt.date())
     
     # Change str to Date
-    if lf.select(date_col).dtypes[0] == pl.Utf8:
-        return lf.with_columns(pl.col(date_col).str.strptime(pl.Datetime, format=format).dt.date())
+    if dtype == pl.Utf8:
+        # Remove the time part
+        lf = lf.with_columns(pl.col(date_col).str.split(' ').list.first())
+
+        # Parse date if format can be parsed automatically
+        try:
+            lf = lf.with_columns(pl.col(date_col).str.strptime(pl.Date))
+            failed = False
+        # If not then manual search
+        except:
+            for format in formats:
+                try:
+                    lf = lf.with_columns(pl.col(date_col).str.strptime(pl.Date, format=format))
+                    failed = False
+                except:
+                    failed = True
     
+        if failed:
+            # No date format that works
+            raise ValueError('Wrong data format.')
+
+        # Check if BCE year -> turn into AD
+        if lf.filter(pl.col(date_col).dt.year() > 2400).height == lf.height:
+            lf = lf.with_columns(pl.col(date_col).dt.offset_by('-543y'))
+        
+        return lf
+
     # Other types not accepted
     raise TypeError('Wrong dtype.')
 
+    
+    
 def clip_dates(lf:pl.LazyFrame, date_col: str, start_year: int = None, start_month: int = 1, start_day: int = 1, end_year: int = None, end_month: int = 12, end_day: int = 31) -> pl.LazyFrame:
     '''Clip dates in a single pl.Date column in a DataFrame/LazyFrame.'''
     assert lf.select(pl.col(date_col)).dtypes[0] == pl.Date
@@ -53,3 +80,14 @@ def clip_dates(lf:pl.LazyFrame, date_col: str, start_year: int = None, start_mon
     if end_year is not None:
         lf = lf.filter(pl.col(date_col) <= pl.date(year=end_year, month=end_month, day=end_day))
     return lf
+
+def identify_in_list(lf: pl.LazyFrame, col_name: str, criteria: list[str], task: str) -> pl.LazyFrame:
+    '''Identifies all entries in column {col_name} with starting string in the list {criteria}.'''
+    search_re = '^' + '|^'.join(criteria)
+    return (
+        lf
+        # Read only certain columns for speed
+        .select(['ENC_HN', 'D001KEY', col_name])
+        # Choose required ICD-10
+        .filter(pl.col(col_name).str.contains(search_re))
+    )
